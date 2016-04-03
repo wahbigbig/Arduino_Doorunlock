dc852650939251b8e93b48bfdf36038d98f84cf8
wahbigbig <wahbigbig@gmail.com>
2016-04-03 16:46:43 +0800

    BT error handling

--- unlock_door.py
+++ unlock_door.py
@@ -4,20 +4,35 @@ import requests
 import bluetooth
 import time
 
-server_address = "http://10.1.1.3:3000" 
-bd_addr = "30:14:12:04:30:52" 
-port = 1
-sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
-sock.connect((bd_addr,port))
- 
+def connectBT():
+	bd_addr = "30:14:12:04:30:52" 
+	port = 1
+	try:
+		sock = bluetooth.BluetoothSocket (bluetooth.RFCOMM)
+		sock.connect((bd_addr,port))
+	except bluetooth.btcommon.BluetoothError as err:
+		print err
+		print "BT Error, wait 5sec to retry"
+		time.sleep(5)
+		connectBT()
+	return sock
+
+sock = connectBT()
+server_address = "http://localhost:3000" 
+
 while 1:
 	r = requests.get(server_address + "/status")
 	if r.content == "1":
 		print "unlock"
 		requests.get(server_address + "/lock")
-		sock.send("1")
+		try:	
+			sock.send("1")
+		except bluetooth.btcommon.BluetoothError:
+			print "Cannot send signal, retry connect"
+			connectBT()
 	else:	
 		print "nothing to do"
 	time.sleep(1)
  
 sock.close()
+
